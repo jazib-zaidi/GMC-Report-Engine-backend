bc5dbedcb5b0f2a1ca63de303c856926d1829210
jazib-zaidi <jazib@feedops.com>
2025-04-28 11:08:32 +0530

    AI-aded

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -3,7 +3,7 @@ const { oauth2Client, google } = require('../utils/googleClient');
 exports.fetchReports = async (req, res) => {
   try {
     const { tokens } = req.token;
-    const { gmcAccountId, date } = req.body;
+    const { gmcAccountId, date, filter } = req.body;
 
     if (!gmcAccountId) {
       return res
@@ -58,13 +58,14 @@ exports.fetchReports = async (req, res) => {
       })
       .join(',');
 
-    const getAllResults = async (gmcAccountId, startDate, endDate) => {
-      const currentQuery = `
+    const getAllResults = async (gmcAccountId, startDate, endDate, filter) => {
+      let currentQuery = `
         SELECT segments.offer_id, segments.date, segments.title, segments.brand, ${productType}, ${customLabel},
                metrics.clicks, metrics.impressions, metrics.ctr, metrics.conversions
         FROM MerchantPerformanceView
         WHERE segments.date BETWEEN '${startDate}' AND '${endDate}'
           AND segments.program = 'FREE_PRODUCT_LISTING'
+
       `;
 
       const previousYearStart = new Date(startDate);
@@ -73,7 +74,7 @@ exports.fetchReports = async (req, res) => {
       const previousYearEnd = new Date(endDate);
       previousYearEnd.setFullYear(previousYearEnd.getFullYear() - 1);
 
-      const previousQuery = `
+      let previousQuery = `
         SELECT segments.offer_id, segments.date, segments.title, segments.brand,segments.custom_label0,
                metrics.clicks, metrics.impressions, metrics.ctr, metrics.conversions
         FROM MerchantPerformanceView
@@ -82,6 +83,20 @@ exports.fetchReports = async (req, res) => {
         }' AND '${previousYearEnd.toISOString().split('T')[0]}'
           AND segments.program = 'FREE_PRODUCT_LISTING'
       `;
+      function camelToSnakeCase(str) {
+        return str.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
+      }
+      const combinedFilter = () => {
+        return `segments.${camelToSnakeCase(filter.selectedAttribute)} = "${
+          filter.searchValue
+        }"`;
+      };
+
+      if (filter?.selectedAttribute) {
+        console.log(combinedFilter());
+        currentQuery += ` AND ${combinedFilter()}`;
+        previousQuery += ` AND ${combinedFilter()}`;
+      }
 
       const [current, previous] = await Promise.all([
         fetchAllDataRecursively(gmcAccountId, currentQuery),
@@ -92,7 +107,7 @@ exports.fetchReports = async (req, res) => {
     };
 
     const getBrandResults = async (gmcAccountId, startDate, endDate) => {
-      const currentQuery = `
+      let currentQuery = `
         SELECT segments.brand,
                metrics.clicks, metrics.impressions, metrics.ctr, metrics.conversions
         FROM MerchantPerformanceView
@@ -106,7 +121,7 @@ exports.fetchReports = async (req, res) => {
       const previousYearEnd = new Date(endDate);
       previousYearEnd.setFullYear(previousYearEnd.getFullYear() - 1);
 
-      const previousQuery = `
+      let previousQuery = `
         SELECT  segments.brand,
                metrics.clicks, metrics.impressions, metrics.ctr, metrics.conversions
         FROM MerchantPerformanceView
@@ -115,6 +130,20 @@ exports.fetchReports = async (req, res) => {
         }' AND '${previousYearEnd.toISOString().split('T')[0]}'
           AND segments.program = 'FREE_PRODUCT_LISTING'
       `;
+      function camelToSnakeCase(str) {
+        return str.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
+      }
+      const combinedFilter = () => {
+        return `segments.${camelToSnakeCase(filter.selectedAttribute)} = "${
+          filter.searchValue
+        }"`;
+      };
+
+      if (filter?.selectedAttribute) {
+        console.log(combinedFilter());
+        currentQuery += ` AND ${combinedFilter()}`;
+        previousQuery += ` AND ${combinedFilter()}`;
+      }
 
       const [current, previous] = await Promise.all([
         fetchAllDataRecursively(gmcAccountId, currentQuery),
@@ -130,7 +159,7 @@ exports.fetchReports = async (req, res) => {
       endDate,
       type
     ) => {
-      const currentQuery = `
+      let currentQuery = `
         SELECT segments.${type},
                metrics.clicks, metrics.impressions, metrics.ctr, metrics.conversions
         FROM MerchantPerformanceView
@@ -144,7 +173,7 @@ exports.fetchReports = async (req, res) => {
       const previousYearEnd = new Date(endDate);
       previousYearEnd.setFullYear(previousYearEnd.getFullYear() - 1);
 
-      const previousQuery = `
+      let previousQuery = `
         SELECT  segments.${type},
                metrics.clicks, metrics.impressions, metrics.ctr, metrics.conversions
         FROM MerchantPerformanceView
@@ -153,7 +182,20 @@ exports.fetchReports = async (req, res) => {
         }' AND '${previousYearEnd.toISOString().split('T')[0]}'
           AND segments.program = 'FREE_PRODUCT_LISTING'
       `;
+      function camelToSnakeCase(str) {
+        return str.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
+      }
+      const combinedFilter = () => {
+        return `segments.${camelToSnakeCase(filter.selectedAttribute)} = "${
+          filter.searchValue
+        }"`;
+      };
 
+      if (filter?.selectedAttribute) {
+        console.log(combinedFilter());
+        currentQuery += ` AND ${combinedFilter()}`;
+        previousQuery += ` AND ${combinedFilter()}`;
+      }
       const [current, previous] = await Promise.all([
         fetchAllDataRecursively(gmcAccountId, currentQuery),
         fetchAllDataRecursively(gmcAccountId, previousQuery),
@@ -232,7 +274,8 @@ exports.fetchReports = async (req, res) => {
     const { current, previous } = await getAllResults(
       gmcAccountId,
       date.startDate,
-      date.endDate
+      date.endDate,
+      filter
     );
 
     const summarize = (data) => {
