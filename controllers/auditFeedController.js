1cac1e31e8b38b8d1e1f16da51397d8074ceb15d
jazib-zaidi <jazib@feedops.com>
2025-10-03 01:14:35 +0530

    feat: Optimized Backend

--- controllers/auditFeedController.js
+++ controllers/auditFeedController.js
@@ -18,7 +18,26 @@ async function fetchAllProductStatuses(content, merchantId) {
     const items = res?.data?.resources || [];
     allItems.push(...items);
     pageToken = res?.data?.nextPageToken || null;
-  } while (false);
+  } while (pageToken);
+
+  return allItems;
+}
+
+async function fetchAllProductsList(content, merchantId) {
+  let allItems = [];
+  let pageToken = null;
+
+  do {
+    const res = await content.products.list({
+      merchantId,
+      pageToken: pageToken || undefined,
+      maxResults: 250,
+    });
+
+    const items = res?.data?.resources || [];
+    allItems.push(...items);
+    pageToken = res?.data?.nextPageToken || null;
+  } while (pageToken);
 
   return allItems;
 }
@@ -71,12 +90,6 @@ exports.googleProductsAudit = async (req, res) => {
     // Fetch ALL pages
     const items = await fetchAllProductStatuses(content, gmcAccountId);
 
-    // to get product add products.list
-
-    // const prod = await content.products.list({
-    //   merchantId: gmcAccountId,
-    // });
-    // console.log(prod);
     function formatDate(date) {
       return date.toISOString().split('T')[0]; // YYYY-MM-DD
     }
@@ -85,31 +98,15 @@ exports.googleProductsAudit = async (req, res) => {
     const start = new Date();
     start.setDate(end.getDate() - 30); // go back 30 days
 
-    console.log(start);
-    console.log(end);
     let query = `
         SELECT segments.offer_id,segments.title, segments.brand,
-               metrics.clicks, metrics.impressions
+               metrics.clicks, metrics.impressions, metrics.ctr
         FROM MerchantPerformanceView
         WHERE segments.date BETWEEN '${formatDate(start)}' AND '${formatDate(
       end
     )}'
       `;
     const response = await fetchAllDataRecursively(gmcAccountId, query);
-
-    console.log(response);
-    // for (let i = 0; i < (prod.data.resources || []).length; i++) {
-    //   const missingAttributes = await findMissingGMCAttributes(
-    //     prod.data.resources[i]
-    //   );
-    //   console.log(missingAttributes);
-    // }
-    // console.log(prod.data.resources);
-
-    // Fetch ALL pages
-    // const items = await fetchAllProductStatuses(content, gmcAccountId, {
-    //   delay: delayMs,
-    // });
 
     return res.json({
       message: 'Fetched all product statuses',
@@ -119,7 +116,31 @@ exports.googleProductsAudit = async (req, res) => {
       productMetrics: response,
     });
   } catch (err) {
-    console.error('Error in googleProductsAudit handler:', err);
+    return res.status(500).json({
+      message: 'Failed to fetch product statuses',
+      error: err.message,
+    });
+  }
+};
+
+exports.fetchAllProducts = async (req, res) => {
+  try {
+    const gmcAccountId = Number(req.query.merchantId) || 238564716;
+
+    const { tokens } = req.token;
+
+    oauth2Client.setCredentials(tokens);
+    const content = google.content({ version: 'v2.1', auth: oauth2Client });
+    // Fetch ALL pages
+    const items = await fetchAllProductsList(content, gmcAccountId);
+
+    return res.json({
+      message: 'Fetched all product',
+      merchantId: gmcAccountId,
+      totalItems: items.length,
+      items,
+    });
+  } catch (err) {
     return res.status(500).json({
       message: 'Failed to fetch product statuses',
       error: err.message,
--- routes/audit.js
+++ routes/audit.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { googleProductsAudit } = require('../controllers/auditFeedController');
+const {
+  googleProductsAudit,
+  fetchAllProducts,
+} = require('../controllers/auditFeedController');
 
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/audit-feed', authMiddleware, googleProductsAudit);
+router.get('/fetch-all-products', authMiddleware, fetchAllProducts);
 
 module.exports = router;
